d0db94f175d735ef2dc5e9ee6fbac5773879e9bd
Michel Lang <michellang@gmail.com>
2019-11-26 10:52:57 +0100

    ...

--- R/LearnerRegrLM.R
+++ R/LearnerRegrLM.R
@@ -12,13 +12,28 @@
 #' ```
 #'
 #' @description
-#' Ordinary linear regression.
-#' Calls [stats::lm()].
+#' Ordinary linear regression. Calls [stats::lm()].
+#'
+#' The importance scores are calculated using `-log10(p)` where `p` is the \eqn{p}-value.
+#' This transformation is necessary to ensure numerical stability for very small \eqn{p}-values.
 #'
 #' @export
 #' @template seealso_learner
 #' @templateVar learner_name regr.lm
-#' @template example
+#' @examples
+#' library(mlr3)
+#' learner = lrn("regr.lm")
+#' print(learner)
+#'
+#' # available parameters:
+#' learner$param_set$ids()
+#'
+#' task = tsk("mtcars")
+#' learner$train(task)
+#' summary(learner$model)
+#'
+#' pred = learner$predict(task, row_ids = head(task$row_ids))
+#' pred
 LearnerRegrLM = R6Class("LearnerRegrLM", inherit = LearnerRegr,
   public = list(
     initialize = function() {
@@ -26,7 +41,7 @@ LearnerRegrLM = R6Class("LearnerRegrLM", inherit = LearnerRegr,
         id = "regr.lm", ,
         predict_types = c("response", "se"),
         feature_types = c("integer", "numeric", "factor"),
-        properties = "weights",
+        properties = c("weights", "importance"),
         packages = "stats",
         man = "mlr3learners::mlr_learners_regr.lm"
       )
@@ -50,6 +65,16 @@ LearnerRegrLM = R6Class("LearnerRegrLM", inherit = LearnerRegr,
         pred = predict(self$model, newdata = newdata, se.fit = TRUE)
         PredictionRegr$new(task = task, response = pred$fit, se = pred$se.fit)
       }
+    },
+
+    importance = function() {
+      if (is.null(self$model)) {
+        stopf("No model stored")
+      }
+
+      p = summary(self$model)$coefficients[, 4L]
+      p = p[names(p) != "(Intercept)"]
+      sort(-log10(p), decreasing = TRUE)
     }
   )
 )
